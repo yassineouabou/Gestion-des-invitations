952fa06db7ea20e90c5bdda72fc577ab99bc2fe4
Yassine OUABOU <100864534+yassineouabou@users.noreply.github.com>
2025-06-10 23:11:00 +0100

    add Organisateur model

---
+++ api/api/Models/Organisateur.cs
@@ -0,0 +1,11 @@
+﻿namespace api.Models
+{
+    public class Organisateur
+    {
+        public long Id { get; set; }
+        public string Nom { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+}

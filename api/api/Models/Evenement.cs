59e24e896459830719e7a9afc12dcdf9214dbb52
Yassine OUABOU <100864534+yassineouabou@users.noreply.github.com>
2025-06-10 23:24:05 +0100

    add Evenement model

---
+++ api/api/Models/Evenement.cs
@@ -0,0 +1,12 @@
+﻿namespace api.Models
+{
+    public class Evenement
+    {
+        public long Id { get; set; }
+        public string Titre { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime DateEvenement { get; set; }
+        public string Lieu { get; set; } = string.Empty;
+        public string Lien { get; set; } = string.Empty;
+    }
+}

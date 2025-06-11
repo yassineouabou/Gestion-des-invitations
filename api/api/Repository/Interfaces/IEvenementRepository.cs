17d3ef0ea104934d2c0ca69b63ba1833fc81b0d1
Yassine OUABOU <100864534+yassineouabou@users.noreply.github.com>
2025-06-11 19:36:30 +0100

    add IEvenementRepository

--- api/api/Repository/Interfaces/IEvenementRepository.cs
+++ api/api/Repository/Interfaces/IEvenementRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace api.Repository.Interfaces
+﻿using api.Dtos.Evenement;
+using api.Models;
+
+namespace api.Repository.Interfaces
 {
     public interface IEvenementRepository
     {
+        Task<Evenement> sava(Evenement evenement);
+        Task<Evenement?> FindById(long id);
+        Task<Evenement?> delete(long id);
+        Task<Evenement> update(long id, CreateEvenement createEvenement);
     }
 }

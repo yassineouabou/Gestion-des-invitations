887d8839aba43e17b3e08d1ff89180806a55e9fe
Yassine OUABOU <100864534+yassineouabou@users.noreply.github.com>
2025-06-13 01:38:27 +0100

    add getAllByOrganisateurId repo

--- api/api/Repository/VerificationRepository.cs
+++ api/api/Repository/VerificationRepository.cs
@@ -1,6 +1,7 @@
 ﻿using api.Data;
 using api.Models;
 using api.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Repository
 {
@@ -13,6 +14,16 @@ namespace api.Repository
             this.appDbContext = appDbContext;
         }
 
+        public async Task<List<Verification>> getAllByOrganisateurId(long organisateurId)
+        {
+            var verifications = await appDbContext.Verifications
+                .Include(v => v.Evenement)
+                .Include(v => v.Visiteur)
+                .Where(v => v.Evenement != null && v.Evenement.OrganisateurId == organisateurId)
+                .ToListAsync();
+
+            return verifications;
+        }
         public async Task<Verification> save(Verification verification)
         {
             await appDbContext.Verifications.AddAsync(verification);
